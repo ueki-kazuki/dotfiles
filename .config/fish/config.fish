878f704572ea3795fa268873c3a989cac5417c1b
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2019-02-11 11:49:44 +0900

    Merge macos config.fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-#set -g PATH "/home/linuxbrew/.linuxbrew/bin:$PATH"
+set fish_prompt_hg_status_added "+"
+set fish_color_autosuggestion brblack
+set -x PATH /usr/local/bin $PATH
+. (pyenv init - | psub)
+. (rbenv init - | psub)
+eval (direnv hook fish)
 
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \c] peco_select_ghq_repository
+end
