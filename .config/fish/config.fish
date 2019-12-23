1f4fe0332e528dac51a341c8ec8a0bf9adf779a5
Kazuki Ueki <kazuki@joetsu.ne.jp>
2019-12-23 17:44:21 +0900

    Fix peco_select_history

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# set -x LANG ja_JP.utf8
 set fish_prompt_hg_status_added "+"
 set fish_color_autosuggestion brblack
 set -x PATH /usr/local/bin $PATH
@@ -6,6 +7,19 @@ set -x PATH /usr/local/bin $PATH
 eval (direnv hook fish)
 
 function fish_user_key_bindings
-  bind \cr 'peco_select_history (commandline -b)'
+  bind \cr peco_select_history (commandline -b)
   bind \c] peco_select_ghq_repository
 end
+
+# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
